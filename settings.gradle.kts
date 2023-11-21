b0eb51f57b2f1ee60ef76f75ba8263af14c899f9
jasser <j.floresh@alumnos.santotomas.cl>
2023-11-21 18:31:00 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Firebase_Tem_Hum"
+include(":app")
